04fd50842a941782c6014202e3a046c3fe2ddf28
leonm <marrakesh94@gmail.com>
2020-05-05 21:05:58 +0200

    Added kernels

--- kerneli.py
+++ kerneli.py
@@ -5,3 +5,21 @@ kernel = np.array(
      [0., 1., 0.],
      [1., 0., 1.]]
 )
+
+konvolucijski_kernel = np.array(
+    [[-1., -1., -1.],
+     [-1., 8., -1.],
+     [-1., -1., -1.]]
+)
+
+horizontalni_kernel = np.array(
+    [[-1., -1., -1.],
+     [0., 0., 0.],
+     [1., 1., 1.]]
+)
+
+vertikalni_kernel = np.array(
+    [[-1., 0., 1.],
+     [-1., 0., 1.],
+     [-1., 0., 1.]]
+)
