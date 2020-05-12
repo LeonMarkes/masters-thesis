f0882a7bdee9cd81a3d8e23ad70b895bcac47d8e
leonm <marrakesh94@gmail.com>
2020-05-12 22:45:24 +0200

    Added filters

--- kerneli.py
+++ kerneli.py
@@ -23,3 +23,13 @@ vertikalni_kernel = np.array(
      [-1., 0., 1.],
      [-1., 0., 1.]]
 )
+
+filteri = np.zeros((2, 3, 3))
+
+filteri[0, :, :] = np.array([[[-1, 0, 1],
+                              [-1, 0, 1],
+                              [-1, 0, 1]]])
+
+filteri[1, :, :] = np.array([[[1, 1, 1],
+                              [0, 0, 0],
+                              [-1, -1, -1]]])
