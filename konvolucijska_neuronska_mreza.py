1526bface6b99168f5a122d6c8a8879c4c31580f
leonm <marrakesh94@gmail.com>
2020-05-13 16:48:02 +0200

    Added method for first convolutional layer

--- konvolucijska_neuronska_mreza.py
+++ konvolucijska_neuronska_mreza.py
@@ -1,17 +1,19 @@
 import numpy as np
 from PIL import Image
-from kerneli import konvolucijski_kernel
+from kerneli import konvolucijski_kernel, konvolucijski_filteri
 from typing import List
+import sys
 
 
 class Konvolucijska_neuronska_mreza():
 
     def __init__(self, naziv_slike: str, kernel: np.ndarray = konvolucijski_kernel) -> None:
         self.slika: Image = self.ucitaj_sliku(naziv_slike)
+        self.mapa_svojstava: np.ndarray = self.konv(self.slika, konvolucijski_filteri)
+        self.mapa_svojstava_relu = self.relu(self.mapa_svojstava)
         self.kernel: np.ndarray = kernel
         self.udruzena_slika: Image = self.udruzi_sliku()
         self.znacajke: np.ndarray = self.trazenje_znacajki()
-
 
     def ucitaj_sliku(self, naziv_slike: str) -> Image:  # vraća crno bijelu sliku
         return Image.open(naziv_slike, 'r').convert('L')
@@ -64,9 +66,43 @@ class Konvolucijska_neuronska_mreza():
 
         return self.pretvori_u_niz(konvoluirane_znacajke)
 
-    def pretvori_u_1d_niz(self) -> List[float]: # vraća jednodimenzionalni niz za ulaz u neuronsku mrežu
+    def konv(self, slika: Image, konvolucijski_filteri: np.ndarray) -> np.ndarray:
+        if len(slika.shape) > 2 or len(konvolucijski_filteri) > 3:
+            print('Error: Nisu unesene dobre dimenzije filtera i slike.')
+            sys.exit()
+        if slika.shape[-1] != konvolucijski_filteri.shape[-1]:
+            print('Error: Slika i filter nisu iste dimenzije.')
+            sys.exit()
+        if konvolucijski_filteri.shape[1] != konvolucijski_filteri.shape[2]:
+            print('Error: Filter nije kvadratna matrica.')
+            sys.exit()
+        if konvolucijski_filteri.shape[1] % 2 == 0:
+            print('Error: Filteri moraju imati neparni broj vrijednosti.')
+            sys.exit()
+
+        mapa_znacajki = np.zeros((slika.shape[0] - konvolucijski_filteri.shape[1] + 1,
+                                  slika.shape[1] - konvolucijski_filteri.shape[1] + 1,
+                                  konvolucijski_filteri.shape[0]))
+
+        for broj_filtera in range(konvolucijski_filteri.shape[0]):
+            print('Filter ', broj_filtera + 1)
+            trenutni_filter = konvolucijski_filteri[broj_filtera, :]
+            if len(trenutni_filter.shape) > 2:
+                konvolucijska_mapa = self.konvolucija(slika[:, :, 0], trenutni_filter[:, :, 0])
+                for ch_num in range(1, trenutni_filter.shape[-1]):
+                    konvolucijska_mapa = konvolucijska_mapa + self.konvolucija(
+                        slika[:, :, ch_num], trenutni_filter[:, :, ch_num])
+            else:
+                konvolucijska_mapa = self.konvolucija(slika, trenutni_filter)
+            mapa_znacajki[:, :, broj_filtera] = konvolucijska_mapa
+        return mapa_znacajki
+
+
+
+    def pretvori_u_1d_niz(self) -> List[float]:  # vraća jednodimenzionalni niz za ulaz u neuronsku mrežu
         return self.znacajke.ravel().tolist()
 
-konv: Konvolucijska_neuronska_mreza = Konvolucijska_neuronska_mreza('bambi.png')
-print(konv.pretvori_u_1d_niz())
-print(len(konv.pretvori_u_1d_niz()))
+
+# konv: Konvolucijska_neuronska_mreza = Konvolucijska_neuronska_mreza('bambi.png')
+# print(konv.pretvori_u_1d_niz())
+# print(len(konv.pretvori_u_1d_niz()))
