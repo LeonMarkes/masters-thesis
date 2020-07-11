91529a4e73d121e6ef43c2e9c6486818f3e4496c
leonm <marrakesh94@gmail.com>
2020-07-11 19:05:22 +0200

    Changed run variables

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ import numpy as np
 
 
 def main():
-    podaci, oznaka = kreiraj_podatkovni_skup('podatkovni_skup')
-    konv_mreza = Konvolucijska_neuronska_mreza(podaci, skriveni_sloj=25, izlazni_sloj=oznaka, stopa_ucenja=.003)
-    konv_mreza.ucenje(3, 3, '350_25_3k')
+    podaci, oznaka = kreiraj_podatkovni_skup('new2')
+    konv_mreza = Konvolucijska_neuronska_mreza(podaci, skriveni_sloj=20, izlazni_sloj=oznaka, stopa_ucenja=.001)
+    # konv_mreza.ucenje(3, 3, '350_25_3k')
     konv_mreza.testiranje()
 
 if __name__ == '__main__':
